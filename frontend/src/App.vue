be3281d6ecd771e214af4a645ae57c1b4f78dfdf
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-11-15 21:15:58 +0630

    frontend backend connection setup done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>Hello world</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  mounted(){
+    this.$axios.get('http://localhost:8000/api/api-testing').then((res)=>{
+      console.log(res.data)
+    })
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
