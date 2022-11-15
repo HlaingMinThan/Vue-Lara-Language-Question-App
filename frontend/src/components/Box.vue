ac6e28d3f88076b63a2aa35f46deaa1094eb96c9
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-11-15 21:59:15 +0630

    finish desktop design

---
+++ frontend/src/components/Box.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="
+      mt-20
+      w-[80%]
+      h-[80%]
+      bg-white
+      flex flex-col
+      justify-center
+      items-center
+      space-y-8
+    "
+  >
+    <img
+      src="https://s3-alpha-sig.figma.com/img/2983/484c/02f1f51da2aaae1b0f190e4b632adfa8?Expires=1669593600&Signature=ait98ugYiA8I31kLgsua6NQ7xCZKWggao8hq4yy5wO1ddMx8zm3LZ1dSZ6QyrcfJo956fpC5Zsrb~s~kIFmPcHEyd2QQCYlELFqy9pCGgx2g8aulRX-8xdpQVBpSekfXQOphUwABG4M6QFEi09H4FcxSNk~nQUcYm5gHEmEY9oGaVIOdPOxjOOxWrscKWFhu9xACPnqpQA6qTVLSgIYV9jJiSGmrbF4WKpiA344FKHqHU1WZqTxov-tki0snyFbCMsX~G0aDS9ArvYNs7Mzru-OtgyxfNFBIbFTlPYYYejZvBqaCcOGyvT4X0VXr0R~QZRdmOxgY2S3guUEs28epSQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+      class="w-[50%]"
+    />
+    <h1 class="text-3xl">happy</h1>
+    <input type="text" class="w-[60%] shadow-md shadow-[#e28a5067] py-3" />
+
+    <button class="px-16 py-4 text-white bg-[#FF6700] text-xl rounded-md">
+      Let's See
+    </button>
+    <div>
+      <img src="image.png" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
